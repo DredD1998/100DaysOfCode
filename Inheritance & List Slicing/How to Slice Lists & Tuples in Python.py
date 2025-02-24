5b8ebd141739cf89aad5120bb91e21408fc16f01
DredD <dreddxf@gmail.com>
2025-02-24 16:59:16 +0530

    committed

---
+++ Inheritance & List Slicing/How to Slice Lists & Tuples in Python.py
@@ -0,0 +1,6 @@
+piano_keys = ["a","b","c","d","e","f","g"]
+print(piano_keys[2:7:2])
+print(piano_keys[:7:3])
+print(piano_keys[::2])
+print(piano_keys[::-1])
+
--- snake_game.py/project15.py
+++ snake_game.py/project15.py
@@ -1,5 +1,7 @@
 from turtle import Turtle , Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 screen = Screen()
 screen.setup(width=600,height=600)
@@ -8,6 +10,11 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+
+
 screen.listen()
 screen.onkey(snake.up,"w")
 screen.onkey(snake.down,"s")
@@ -21,7 +28,21 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-   
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
+    
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
 
 
 

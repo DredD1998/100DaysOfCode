0fbd0f716dd94c0d5c683c48a7857d8fff4f71d8
DredD <dreddxf@gmail.com>
2025-03-07 18:23:13 +0530

    first commit

--- NATO_alphabet_start/project18.py/main.py
+++ NATO_alphabet_start/project18.py/main.py
@@ -1,7 +1,7 @@
 import pandas
 data = pandas.read_csv("NATO_alphabet_start/project18.py/nato_phonetic_alphabet.csv")
 
-new_dict = {row.letter:row.code for (index,row) in data.iterrows()}
+new_dict = {value.letter:value.code for (key,value) in data.iterrows()}
 word = input("Enter a word: ").upper()
 new_data = [new_dict[letter] for letter in word ]
 print(new_data)
